a53ae680b20eea61fd49cd1eb955265d352134d8
Dave H <githeeschen@gmail.com>
2020-11-24 16:24:53 -0500

    initial commit

---
+++ src/routes/home/index.js
@@ -0,0 +1,15 @@
+import { h } from 'preact';
+import style from './style.css';
+
+const Home = () => (
+	<div class='content-container'>
+		<div class='left'>
+
+		</div>
+		<div class='right'>
+			<button class='btn'> Start </button>
+		</div>
+	</div>
+);
+
+export default Home;
